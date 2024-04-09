c1ca2a2375b26b57105d1211413670addfb5f4d1
viktor <viktorwv@icloud.com>
2024-04-09 13:45:04 +0200

    :sparkles: add broadcast

--- src/server/main.c
+++ src/server/main.c
@@ -3,24 +3,70 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
+#define MAX_MESSAGE_SIZE 1024
+
 // Structure to store data for each WebSocket session
 struct per_session_data {
+    int role;
+    int message_sent;
+    int last_sent;
 };
 
+struct GLOBAL_DATA {
+    int packet_id;
+    int total_clients;
+    char message[MAX_MESSAGE_SIZE];
+} global_data = {0};
+
 // Callback function for WebSocket server messages
-int callback(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len) {
+int callback(struct lws *wsi, enum lws_callback_reasons reason, struct per_session_data *user, void *in, size_t len) {
     switch (reason) {
         case LWS_CALLBACK_ESTABLISHED: // Handle new connection event
             // Add a timer to send a timestamp every second
+            user->role = 0;
+            user->message_sent = 0;
+            user->last_sent = 0;
+            global_data.total_clients++;
             lws_callback_on_writable(wsi);
             break;
+
+        case LWS_CALLBACK_RECEIVE: // Handle incoming data event
+            // Print the received message
+            char* message = (char *)malloc(len + 1);
+            strncpy(message, in, len);
+            message[len] = '\0';
+
+
+            if (strcmp(message, "PASSWORD") == 0) {
+                user->role = 1;
+            } else if (user->role){
+                global_data.packet_id++;
+                sprintf(global_data.message, "%d: %s", global_data.packet_id, message);
+                lws_callback_on_writable_all_protocol(lws_get_context(wsi), lws_get_protocol(wsi));
+            }
+
+            printf("Received message(%d): %s\n",user->message_sent, message);
+            user->message_sent++;
+
+            break; 
+
         case LWS_CALLBACK_SERVER_WRITEABLE: // Handle send data event
             // Send the timestamp
-            time_t t = time(NULL);
-            char timestamp[20];
-            sprintf(timestamp, "%ld", t);
-            lws_write(wsi, timestamp, strlen(timestamp), LWS_WRITE_TEXT);
+            if (user->last_sent != global_data.packet_id) {
+                lws_write(wsi, global_data.message, strlen(global_data.message), LWS_WRITE_TEXT);
+                user->last_sent = global_data.packet_id;
+            } else {           
+                time_t t = time(NULL);
+                char timestamp[20];
+                sprintf(timestamp, "%ld", t);
+                lws_write(wsi, timestamp, strlen(timestamp), LWS_WRITE_TEXT);
+            }
             //lws_callback_on_writable(wsi);
+            break;
+        
+        case LWS_CALLBACK_CLOSED: // Handle close connection event
+            global_data.total_clients--;
             break;
         // Other events omitted
     }
@@ -28,6 +74,8 @@ int callback(struct lws *wsi, enum lws_callback_reasons reason, void *user, void
 }
 
 int main(int argc, char **argv) {
+    global_data.total_clients = 0;
+    global_data.packet_id = 0;
     // Create the WebSocket protocol
     static struct lws_protocols protocols[] = {
         {
